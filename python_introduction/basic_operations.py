a2b7c0f7cf86d8a061297bf6d68a22c2352f0434
Evans Torddey <ea.torddey@gmail.com>
2024-06-20 16:03:24 +0000

    Updated file

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,5 @@
+number1 = 10
+number2 = 5
+print ("Addition of 10 and 5 is", int(number1 + number2))
+print ("Subtraction of 10 and 5 is", int(number1 - number2))
+print ("Multiplication of 10 and 5 is", int(number1 * number2))
