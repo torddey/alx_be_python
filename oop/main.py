a26e4448a70af20da28d1a23ec8b356258964d0e
Evans Torddey <ea.torddey@gmail.com>
2024-07-13 21:04:45 +0000

    Initial commit

--- oop/main.py
+++ oop/main.py
@@ -1,3 +1,12 @@
 class Item:
-    pass
+    def __init__(self, name, price, quantity):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+    def calculate_total_price(self, x, y):
+        return self.price * self.quantity
+        
+
     
+
