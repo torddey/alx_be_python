aaa88a3fba6ac8a6119b960ff4633af580cab0bb
Evans Torddey <ea.torddey@gmail.com>
2024-07-20 19:30:50 +0000

    update

--- oop/main.py
+++ oop/main.py
@@ -2,7 +2,7 @@ from book_class import Book
 
 def main():
  
-    my_book = Book("IDGAF", "Donald Trump", 1995)
+    my_book = Book("1984", "George Orwell", 1949)
     print(my_book)  # Expected to use __str__
     print(repr(my_book))  # Expected to use __repr__
     del my_book
