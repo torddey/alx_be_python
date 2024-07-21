154b07b21f9f301102ce7c34bf34ae348fd26776
Evans Torddey <ea.torddey@gmail.com>
2024-07-21 00:53:58 +0000

    update

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -6,7 +6,7 @@ class Shape:
 
 class Rectangle(Shape):
 
-    def __init__(self, lenght= int, width= int):
+    def __init__(self, length= int, width= int):
         self.length = length
         self.width = width
 
